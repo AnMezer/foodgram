0c0264a682120074a7cfa958f2d6dec611c3245e
unknown <an.mezer@yandex.ru>
2026-03-09 13:58:05 +0700

    feat:init foodgram project, add CustomUser model

---
+++ backend/users/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class CustomUser(AbstractUser):
+    avatar = models.ImageField('Аватар', upload_to='avatars', blank=True)
+    email = models.EmailField(unique=True, max_length=254)
+
+    class Meta:
+        verbose_name = 'пользователь'
+        verbose_name_plural = 'Пользователи'
